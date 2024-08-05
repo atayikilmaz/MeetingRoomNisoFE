d26a4e79f5a71cfa035f265a2fb3bd95ec69bbde
atayikilmaz <ata.yikilmaz@gmail.com>
2024-08-05 16:53:11 +0300

    disable light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body className="gray-700">
         <AuthProvider>
           <Navbar />
